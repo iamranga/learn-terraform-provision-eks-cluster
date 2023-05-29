6ef0ce6b081277d9591bc197ef3b23f2ffbae29f
iamranga <rangarajan.ramadass@cognizant.com>
2023-05-30 04:23:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,5 +4,13 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "us-east-1"
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
 }
