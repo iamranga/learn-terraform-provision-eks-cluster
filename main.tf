4779991dbfd0f25fd3b1f4cd6ff7c7cf17f07ac1
iamranga <rangarajan.ramadass@cognizant.com>
2023-05-30 04:21:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "aws-eks-terraform-morpheus"
 }
 
 resource "random_string" "suffix" {
